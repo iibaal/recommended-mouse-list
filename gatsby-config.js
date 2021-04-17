77f1034dfd8593c72e0b6144a783c5c742055c10
dogepism <blazinteostra@gmail.com>
2021-04-17 08:49:38 +0700

    added googleanalytics gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,16 @@ module.exports = {
       },
       __key: "pages",
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-194751837-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      }
+    },
   ],
 };
